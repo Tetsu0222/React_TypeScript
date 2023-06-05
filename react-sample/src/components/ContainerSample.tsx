8995ab9daafc4340bc75edaf6ddc98968c3b7f77
tetsu <root2120@gmail.com>
2023-06-06 06:57:49 +0900

    Reactの型

--- react-sample/src/components/ContainerSample.tsx
+++ react-sample/src/components/ContainerSample.tsx
@@ -1,16 +1,20 @@
-//コンポーネントを使用する際は、開始タグと閉じタグで他の要素やコンポーネントを囲むことができる。
-//この場合は、propsの中のchirdrenにその要素が渡される。
 
-//以下のコードでは、ParentコンポーネントでContainerを使用する際に、p要素を囲んでいる。
-//この囲んだp要素がContainerのchildrenに与えられる。
+//Reactにおける型
+//引数のpropsへ注釈を加えることで、親コンポーネントから与えることのできる値を制限できる。
+//childrenを取る場合、型はReact.ReactNodeを指定する。
 
 
-//Containerは赤背景のボックスの中に、タイトルと子要素を表示する。
-const Container = ( props:{ title:string ; children:React.ReactElement }) => {
+//Containerのpropsの型を定義する。
+type ContainerProps = {
+    title:string
+    children:React.ReactNode
+}
+
+//引数のpropsを注釈型へ変更する。
+const Container = ( props:ContainerProps ):JSX.Element => {
         
         const { title , children } = props
-        //propsのchildrenを埋め込むと、このコンポーネントの開始タグと閉じタグで囲んだ要素を表示する。
-        //propsのtitleが文字列、childrenがReactElementになっている。
+
         return (
             <div style={ { background: 'red' }}>
                 <span>{ title }</span>
@@ -19,10 +23,8 @@ const Container = ( props:{ title:string ; children:React.ReactElement }) => {
         )
 }
 
-//Continerを使用する際に、他の要素を囲って使用する。
 const Parent = () => {
 
-    //<p>タグの要素(Containerで囲われたHTML要素)が、Containerのchildrenに渡される。
     return (
         <Container title="Hello">
             <p>ここの部分が背景色で囲まれる。</p>
@@ -32,20 +34,3 @@ const Parent = () => {
 
 
 export default Parent
-
-
-/*
-結果的にHTML要素は以下のようになっている。
-
-<div style="background:red">
-    <span>Hello</span>  //親要素から渡された要素(title)
-    <div>
-        <p>ここの部分が背景色で囲まれる。</p>   //childrenとして親要素から渡された要素
-    </div>
-</div>
-
-以下の内容で型指定している。
-title:string
-children:React.ReactElement
-
-*/
