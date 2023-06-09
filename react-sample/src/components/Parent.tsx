b382bc11b3a96c992e65cbca202c5e879b94bcae
tetsu <root2120@gmail.com>
2023-06-10 07:34:04 +0900

    メモ化コンポーネント2

--- react-sample/src/components/Parent.tsx
+++ react-sample/src/components/Parent.tsx
@@ -1,19 +1,6 @@
 //メモ化コンポーネント
-
-
-//propsや内部状態が更新された時
-//コンポーネント内で参照しているContextの値が更新された時
-//親コンポーネントが再描画された時
-
-//上記の3パターンでコンポーネントが再描画される。
-//親コンポーネントが再描画されると、無条件に子コンポーネントが再描画される。
-//その再描画の伝播を止めるために、メモ化コンポーネントを使用する。
-
-//メモ化コンポーネントは、親コンポーネントで再描画が発生しても
-//propsやContextの値が変化しない場合は、親コンポーネントによる再描画は発生しない。
-
-//メモ化コンポーネントは関数コンポーネントをmemo関数でラップすると作成できる。
-//memo<function> これでメモ化コンポーネントになる。
+//コンポーネントの再描画を抑止することができるが
+//メモ化コンポーネントに関数やオブジェクトを渡すと、また親の再描画によってコンポーネントの再描画が発生する。
 
 import React , { memo , useState } from "react";
 
@@ -21,48 +8,60 @@ type FizzProps = {
     isFizz:boolean
 }
 
-//Fizzは通常の関数コンポーネント
-//isFizzがtrueの場合はFizzと表示、それ以外は何も表示しない。
-//isFizzの変化に関わらず、親が再描画されるとFizzも再描画される。
 const Fizz = ( props:FizzProps ) => {
     const { isFizz } = props
     console.log( 'Fizzが再描画されました , isFizz=${isFizz}' )
     return <span>{isFizz ? 'Fizz' : '' }</span>
 }
 
+//BuzzのpropsにonClick関数（コールバック関数）を追加
 type BuzzProps = {
     isBuzz:boolean
+    onClick:() => void
 }
 
-//Buzzはメモ化した関数コンポーネント
-//isBuzzがtrueの場合はBuzzと表示、それ以外は何も表示しない。
-//親コンポーネントが再描画されても、izBuzzが変化しない限りはBuzzは再描画しない。
+//onClick関数を追加
 const Buzz = memo<BuzzProps>( ( props ) => {
-    const { isBuzz } = props
+    const { isBuzz , onClick } = props
     console.log( 'Buzzが再描画されました , isBuzz=${isBuzz}' )
 
     return (
-        <span>
+        <span onClick={ onClick }>
             {isBuzz ? 'Buzz' : '' }
         </span>
     )
 })
 
-//この形式でexportした時は、import{ Parent } from で読み込む。
 export const Parent = () => {
     const [ count , setCount ] = useState( 1 )
     const isFizz = count % 3 === 0
     const isBuzz = count % 5 === 0
 
+    //この関数はParentの再描画の度に作成される。
+    const onBuzzClick = () => {
+        console.log( 'Buzzがクリックされました , isBuzz=${isBuzz}' )
+    }
+
     console.log( 'Parentが再描画されました,count=${ count }' )
+
+    //onBuzzClickをonClickで起動するように追加
     return (
         <div>
             <button onClick={ () => setCount( ( c ) => c + 1 )}>+1</button>
             <p>{'現在のカウント:${count}'}</p>
             <p>
                 <Fizz isFizz={isFizz} />
-                <Buzz isBuzz={isBuzz} />
+                <Buzz isBuzz={isBuzz} onClick={ onBuzzClick }/>
             </p>
         </div>
     )
 }
+
+
+//以上のコードは、再描画の度に、Parentで新しく作られた関数がBuzzに渡される。
+//そのため、再描画が発生してしまう。
+//その再描画を抑制するには、同じ関数を渡す必要がある。
+//また、オブジェクトや配列などもコンポーネント内で作成すると、描画の度に新しいものが作成されるため、再描画が発生する原因となる。
+
+//useCallbackやuseMemoは、その関数や値をメモ化することが可能
+//これらのフックを利用することで、メモ化コンポーネントに関数やオブジェクトを渡しても、再描画を抑制できる。
