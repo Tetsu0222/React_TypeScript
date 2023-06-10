233df4748ce4967b260309b8b45daf7b4e675402
tetsu <root2120@gmail.com>
2023-06-10 14:50:52 +0900

    ページの種類と確認方法と手法の決定要素

--- next-testsample/pages/sample.tsx
+++ next-testsample/pages/sample.tsx
@@ -9,3 +9,21 @@ export default Sample
 
 //npm run devでテストサーバーを起動
 //表示されたURLに追加して、ファイル名（このファイルであればsample)を追加すると読み込みできる。
+
+//npm run buildでビルドした際に、ページがどのタイプか確認できる。
+//ServerがSSR、StaticとSSGがSSG、ISRはISR
+
+//Next.jsでは実装する関数やその関数の返す値によって、Pagesのレンダリング手法が変わる。
+//レンダリング手法を決定する主な要素はデータ取得の関数
+
+//SSG
+//getStaticProps
+
+//SSR
+//getServerSideProps
+
+//ISR
+//revalidateを返す getStaticProps
+
+//CSR
+//上記以外の任意の関数
