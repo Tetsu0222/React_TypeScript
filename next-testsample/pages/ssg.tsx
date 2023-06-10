f1eb7f286a6ba8ec4f6cd424d9e72a53f8a6a0b2
tetsu <root2120@gmail.com>
2023-06-10 15:22:07 +0900

    getStaticPropsによるSSG実装

--- next-testsample/pages/ssg.tsx
+++ next-testsample/pages/ssg.tsx
@@ -1,19 +1,20 @@
 //SSGによるページの実装
-//getStaticPropsを使用せず、データ取得を一切行わずに実装して生成
+//getStaticPropsを使用
 
-//型のために導入
-import{ NextPage } from 'next'
-
-//Next.jsの組み込みコンポーネント
+import{ GetStaticProps , NextPage , NextPageContext } from 'next'
 import Head from 'next/head'
 
 //ページコンポーネントのpropsの型の定義（ここでは空）
-type SSGProps = {}
+type SSGProps = {
+    message:string
+}
 
-//SSG向けのページを実装
-//NextPageはNext.jsのPages向けの型
-//NextPage<props>でpropsが入るPageであることを明治
-const SSG : NextPage<SSGProps> = () => {
+//SSGはgetStaticPropsが返したpropsを受け取ることができる。
+//Nextpage<SSGProps>はmessage:stringのみを受け取って生成されるページの型
+const SSG : NextPage<SSGProps> = ( props ) => {
+
+    const { message } = props
+
     return (
         <div>
             <Head>
@@ -24,15 +25,38 @@ const SSG : NextPage<SSGProps> = () => {
                 <p>
                     このページは静的サイト生成によってビルド時に生成されたページです。
                 </p>
+                <p>{ message }</p>
             </main>
         </div>
     )
 }
 
+//getStaticPropsはビルド時に実行される。
+//GetStaticProps<SSGProps>はSSGPropsを引数にとるgetStaticPropsの型
+export const getStaticProps : GetStaticProps<SSGProps> = async( context ) => {
+    const timestamp = new Date().toLocaleString()
+    const message = `${timestamp}にgetStaticPropsが実行されました。`
+    console.log( message )
+    return{
+        //ここで返したpropsを元にページコンポーネントを描画する。
+        props:{
+            message ,
+        },
+    }
+}
+
 export default SSG
 
-//NextPageはpagesのための型
-//受け付けるpropsを決め、NextPage<Props>のように指定する。
 
-//npm run buildでビルド実行後、npm run devでサーバー起動
-//ブラウザからアクセスすると実装したページを確認できる。
+//getStaticPropsはエクスポートする必要があり、同時に非同期関数としてasyncとともに定義する必要あり。
+//また、引数にはContextが与えられる。このcontextにはビルド時に使用できるデータも含まれている。
+
+//contextは実行関連の情報がまとまったオブジェクト
+//context.localeのようなカタチでアクセスできる。
+
+//params パスパラメータ　SSGの場合はgetStaticPaths関数を別途定義した時に参照可能
+//locale 現在のローケル情報
+//locales サポートしているローケルの配列
+//defaultLocale デフォルトのローケル
+//preview        Preview Modeかどうか
+//previewData setPreviewDataによってセットされたデータ
