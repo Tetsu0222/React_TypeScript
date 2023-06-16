cb847ad08f710d61cba06081ef9507bf30ffd443
tetsu <root2120@gmail.com>
2023-06-16 20:38:06 +0900

    styled-componentsの適用

--- next-sample2/next.config.js
+++ next-sample2/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  //ここを追加して使用できるようにする。
+  compiler : {
+    styledComponents: true,
+  },
+
+
 }
 
 module.exports = nextConfig
--- next-sample2/pages/_document.tsx
+++ next-sample2/pages/_document.tsx
@@ -1,13 +1,42 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+//SSRやSSG使用時に、サーバーサイドでスタイルを適用させるための設定を記述する。
+//getInitialPropsのメソッドを実装する。
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+//カスタムドキュメントと呼ばれる仕組みで、デフォルトで生成せれるページ設定のうち、htmlやheadなどの要素に関わる部分を上書きする。
+
+
+
+import Document , { DocumentContext } from "next/document";
+import{ ServerStyleSheet } from 'styled-components';
+
+//デフォルトのDoucumentをMyDoucumentで上書き
+export default class MyDocument extends Document {
+  static async getInitialProps( ctx: DocumentContext ){
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try{
+
+      ctx.renderPage = () => originalRenderPage( {
+        enhanceApp : ( App ) => ( props ) => sheet.collectStyles( <App{...props } /> ),
+      })
+
+      //初期値を流用
+      const initialProps = await Document.getInitialProps( ctx )
+
+      //initialPropsに加えてstyleを追加して返す。
+      return{
+        ...initialProps,
+        styles:[
+          //もともとのstyle
+          initialProps.styles,
+          //styled-componentsのstyle
+          sheet.getStyleElement()
+        ],
+      }
+
+    }finally{
+      sheet.seal()
+    }
+
+  }
 }
