44757e43e2912f81a268374d24b4ffe810eba28e
tetsu <root2120@gmail.com>
2023-06-18 13:12:14 +0900

    next.config.jsでstyle-componentsの有効化、_doument.tsxでSSRでもstyled-componentsが動作するよう設定、_app.tsxでグローバルにスタイルを適用させる。

--- nextjs-gihyo-book/next.config.js
+++ nextjs-gihyo-book/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  //styledComponentsの有効化
+  compiler:{
+    styledComponents:true,
+  },
+
 }
 
 module.exports = nextConfig
