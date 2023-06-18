92ac310872ee1de072ba9a3a5e0416a5eaf5e1c1
tetsu <root2120@gmail.com>
2023-06-18 19:47:52 +0900

    StoryBookの設定ファイルの編集

--- nextjs-gihyo-book/.storybook/preview.ts
+++ nextjs-gihyo-book/.storybook/preview.ts
@@ -1,15 +1,53 @@
-import type { Preview } from "@storybook/react";
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle , ThemeProvider } from 'styled-components'
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+export const parameters = {
+  actions : { argTypeRegex : '^on[A-Z].*' },
+  controls : {
+    matchers : {
+      color : /(background|color)$/i,
+      date : /Date$/,
     },
   },
-};
+}
 
-export default preview;
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea{
+    padding : 0;
+    margin : 0;
+    font-family : -apple-system , BlinkMacSystemFont , Segoe UI , Roboto , 0xygen , Ubuntu , Cantarell , Fira Sans , Droid Sans , Helvetica Neue ,sans-serif;
+  }
+  *{
+    box-sizing : border-box;
+  }
+  a{
+    text-decoration : none;
+    transition : .25s;
+    color:#000000;
+  }
+`
+
+//Themeの適用
+addDecorator( ( story ) => (
+  <ThemeProvider theme={ theme }>
+  <GlobalStyle />
+  { story() }
+  </ThemeProvider>
+))
+
+
+//next/imageの差し替え
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty( NextImage , 'default' , {
+  configurable : true,
+  value : ( props ) => typeof props.src === 'string' ? (
+    <OriginalNextImage{ ...props } unoptimized blurDataURL = { props.src } />
+  ) : (
+    <OriginalnextImage{ ...props } unoptimized />
+  ),
+})
